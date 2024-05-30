5a277082ffdd8c3c698df515fb9f549951e7df57
meeyi_zhao <meeyi_zhao@realsil.com.cn>
2024-05-30 15:46:07 +0800

    test

--- scripts/ci/ignore_check_file.py
+++ scripts/ci/ignore_check_file.py
@@ -2,6 +2,6 @@
 
 # Copyright(c) 2023, Realtek Semiconductor Corporation. All rights reserved.
 
-ignore_file_encoding = [] #r"sdk/src/mcu/peripheral/usb",
+ignore_file_encoding = [] #r"sdk/src/mcu/peripheral/usb", 
 
 ignore_file_cpp_astyle_check = []
