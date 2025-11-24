11b13fd04d8902a39baddc9c88566d8896ed241b
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2025-11-24 17:43:36 +0800

    osif: support delete itself in os_task_delete
    1. when the input is NULL in os_task_delete, delete its own task.
    
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

--- bee/rtl87x2g/osif/zephyr/osif_zephyr.c
+++ bee/rtl87x2g/osif/zephyr/osif_zephyr.c
@@ -300,6 +300,11 @@ bool os_task_delete_zephyr(void *p_handle)
 
     obj = (struct k_thread *)p_handle;
 
+    if (obj == NULL)
+    {
+        k_thread_abort(_current);
+    }
+
     k_thread_abort(obj);
     //task signal todo
     return true;
