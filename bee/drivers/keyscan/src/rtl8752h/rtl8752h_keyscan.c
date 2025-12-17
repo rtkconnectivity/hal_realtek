8bb244c64b59da7d162b656dd50e0968fae35b2a
Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>
2025-12-17 16:58:00 +0800

    drivers: kscan: set CR[22] in manual mode after dlps
    Set CR[22] after dlps to trigger a scan in manual scan mode.
    
    Signed-off-by: Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>

--- bee/drivers/keyscan/src/rtl8752h/rtl8752h_keyscan.c
+++ bee/drivers/keyscan/src/rtl8752h/rtl8752h_keyscan.c
@@ -21,16 +21,17 @@
   */
 void KEYSCAN_DLPSEnter(void *PeriReg, void *StoreBuf)
 {
+    KEYSCAN_TypeDef *KSCANx = (KEYSCAN_TypeDef *)PeriReg;
     KEYSCANStoreReg_Typedef *store_buf = (KEYSCANStoreReg_Typedef *)StoreBuf;
 
     RCC_PeriphClockCmd(APBPeriph_KEYSCAN, APBPeriph_KEYSCAN_CLOCK, ENABLE);
 
-    store_buf->keyscan_reg[0] = KEYSCAN->CLKDIV;               /* 0x00 */
-    store_buf->keyscan_reg[1] = KEYSCAN->TIMERCR;              /* 0x04 */
-    store_buf->keyscan_reg[2] = KEYSCAN->CR;                   /* 0x08 */
-    store_buf->keyscan_reg[3] = KEYSCAN->COLCR;                /* 0x0C */
-    store_buf->keyscan_reg[4] = KEYSCAN->ROWCR;                /* 0x10 */
-    store_buf->keyscan_reg[6] = KEYSCAN->INTMASK;              /* 0x18 */
+    store_buf->keyscan_reg[0] = KSCANx->CLKDIV;               /* 0x00 */
+    store_buf->keyscan_reg[1] = KSCANx->TIMERCR;              /* 0x04 */
+    store_buf->keyscan_reg[2] = KSCANx->CR;                   /* 0x08 */
+    store_buf->keyscan_reg[3] = KSCANx->COLCR;                /* 0x0C */
+    store_buf->keyscan_reg[4] = KSCANx->ROWCR;                /* 0x10 */
+    store_buf->keyscan_reg[6] = KSCANx->INTMASK;              /* 0x18 */
 
     return;
 }
@@ -43,19 +44,27 @@ void KEYSCAN_DLPSEnter(void *PeriReg, void *StoreBuf)
   */
 void KEYSCAN_DLPSExit(void *PeriReg, void *StoreBuf, uint32_t scanmode, uint32_t manual_sel)
 {
+    KEYSCAN_TypeDef *KSCANx = (KEYSCAN_TypeDef *)PeriReg;
     KEYSCANStoreReg_Typedef *store_buf = (KEYSCANStoreReg_Typedef *)StoreBuf;
 
     RCC_PeriphClockCmd(APBPeriph_KEYSCAN, APBPeriph_KEYSCAN_CLOCK, ENABLE);
 
     /* Set FSM to idle state */
-    KEYSCAN->CR &= ~BIT31;
-    KEYSCAN->CLKDIV = store_buf->keyscan_reg[0];
-    KEYSCAN->CR      = (store_buf->keyscan_reg[2] & (~(BIT31 | BIT11 | BIT30))) | scanmode | manual_sel;
-    KEYSCAN->TIMERCR = store_buf->keyscan_reg[1];
-    KEYSCAN->COLCR = store_buf->keyscan_reg[3];
-    KEYSCAN->ROWCR = store_buf->keyscan_reg[4];
-    KEYSCAN->INTMASK = store_buf->keyscan_reg[6];
-    KEYSCAN->CR |= (store_buf->keyscan_reg[2] & (~(BIT11 | BIT30))) | BIT31 | scanmode | manual_sel;
+    KSCANx->CR &= ~BIT31;
+    KSCANx->CLKDIV = store_buf->keyscan_reg[0];
+    KSCANx->CR      = (store_buf->keyscan_reg[2] & (~(BIT31 | BIT11 | BIT30))) | scanmode | manual_sel;
+    KSCANx->TIMERCR = store_buf->keyscan_reg[1];
+    KSCANx->COLCR = store_buf->keyscan_reg[3];
+    KSCANx->ROWCR = store_buf->keyscan_reg[4];
+    KSCANx->INTMASK = store_buf->keyscan_reg[6];
+    KSCANx->CR |= (store_buf->keyscan_reg[2] & (~(BIT11 | BIT30))) | scanmode | manual_sel;
+
+    if (((KSCANx->CR & BIT30) == 0) && ((KSCANx->CR & BIT11) == 0))
+    {
+        KSCANx->CR |= BIT22;
+        KSCANx->CR |= BIT31;
+    }
+
 
     return;
 }
--- bee/drivers/keyscan/src/rtl87x2g/rtl87x2g_keyscan.c
+++ bee/drivers/keyscan/src/rtl87x2g/rtl87x2g_keyscan.c
@@ -32,16 +32,17 @@ RAM_FUNCTION
 #endif
 void KEYSCAN_DLPSEnter(void *PeriReg, void *StoreBuf)
 {
+    KEYSCAN_TypeDef *KSCANx = (KEYSCAN_TypeDef *)PeriReg;
     KEYSCANStoreReg_Typedef *store_buf = (KEYSCANStoreReg_Typedef *)StoreBuf;
 
     RCC_PeriphClockCmd(APBPeriph_KEYSCAN, APBPeriph_KEYSCAN_CLOCK, ENABLE);
 
-    store_buf->keyscan_reg[0] = KEYSCAN->KEYSCAN_CLK_DIV;         /* 0x00 */
-    store_buf->keyscan_reg[1] = KEYSCAN->KEYSCAN_CONFIG1;         /* 0x04 */
-    store_buf->keyscan_reg[2] = KEYSCAN->KEYSCAN_CONFIG2;         /* 0x08 */
-    store_buf->keyscan_reg[3] = KEYSCAN->KEYSCAN_COLUMN_CONFIG;   /* 0x0C */
-    store_buf->keyscan_reg[4] = KEYSCAN->KEYSCAN_ROW_CONFIG;      /* 0x10 */
-    store_buf->keyscan_reg[6] = KEYSCAN->KEYSCAN_INT_MASK;        /* 0x18 */
+    store_buf->keyscan_reg[0] = KSCANx->KEYSCAN_CLK_DIV;         /* 0x00 */
+    store_buf->keyscan_reg[1] = KSCANx->KEYSCAN_CONFIG1;         /* 0x04 */
+    store_buf->keyscan_reg[2] = KSCANx->KEYSCAN_CONFIG2;         /* 0x08 */
+    store_buf->keyscan_reg[3] = KSCANx->KEYSCAN_COLUMN_CONFIG;   /* 0x0C */
+    store_buf->keyscan_reg[4] = KSCANx->KEYSCAN_ROW_CONFIG;      /* 0x10 */
+    store_buf->keyscan_reg[6] = KSCANx->KEYSCAN_INT_MASK;        /* 0x18 */
 
     return;
 }
@@ -57,21 +58,28 @@ RAM_FUNCTION
 #endif
 void KEYSCAN_DLPSExit(void *PeriReg, void *StoreBuf, uint32_t scanmode, uint32_t manual_sel)
 {
+    KEYSCAN_TypeDef *KSCANx = (KEYSCAN_TypeDef *)PeriReg;
     KEYSCANStoreReg_Typedef *store_buf = (KEYSCANStoreReg_Typedef *)StoreBuf;
 
     RCC_PeriphClockCmd(APBPeriph_KEYSCAN, APBPeriph_KEYSCAN_CLOCK, ENABLE);
 
     /* Set FSM to idle state */
-    KEYSCAN->KEYSCAN_CONFIG2 &= ~BIT31;
-    KEYSCAN->KEYSCAN_CLK_DIV = store_buf->keyscan_reg[0];
-    KEYSCAN->KEYSCAN_CONFIG2 = (store_buf->keyscan_reg[2] & (~(BIT31 | BIT11 | BIT30))) |
+    KSCANx->KEYSCAN_CONFIG2 &= ~BIT31;
+    KSCANx->KEYSCAN_CLK_DIV = store_buf->keyscan_reg[0];
+    KSCANx->KEYSCAN_CONFIG2 = (store_buf->keyscan_reg[2] & (~(BIT31 | BIT11 | BIT30))) |
+                              (!!scanmode << 30) | (!!manual_sel << 11);
+    KSCANx->KEYSCAN_CONFIG1 = store_buf->keyscan_reg[1];
+    KSCANx->KEYSCAN_COLUMN_CONFIG = store_buf->keyscan_reg[3];
+    KSCANx->KEYSCAN_ROW_CONFIG = store_buf->keyscan_reg[4];
+    KSCANx->KEYSCAN_INT_MASK = store_buf->keyscan_reg[6];
+    KSCANx->KEYSCAN_CONFIG2 |= (store_buf->keyscan_reg[2] & (~(BIT11 | BIT30))) |
                                (!!scanmode << 30) | (!!manual_sel << 11);
-    KEYSCAN->KEYSCAN_CONFIG1 = store_buf->keyscan_reg[1];
-    KEYSCAN->KEYSCAN_COLUMN_CONFIG = store_buf->keyscan_reg[3];
-    KEYSCAN->KEYSCAN_ROW_CONFIG = store_buf->keyscan_reg[4];
-    KEYSCAN->KEYSCAN_INT_MASK = store_buf->keyscan_reg[6];
-    KEYSCAN->KEYSCAN_CONFIG2 |= (store_buf->keyscan_reg[2] & (~(BIT11 | BIT30))) | BIT31 |
-                                (!!scanmode << 30) | (!!manual_sel << 11);
+
+    if (((KSCANx->KEYSCAN_CONFIG2 & BIT30) == 0) && ((KEYSCAN->KEYSCAN_CONFIG2 & BIT11) == 0))
+    {
+        KSCANx->KEYSCAN_CONFIG2 |= BIT22;
+        KSCANx->KEYSCAN_CONFIG2 |= BIT31;
+    }
 
     return;
 }
